20b420124cf21e0eba4e74bbaca9365b3cffd343
PallaviRoy <roypi@mail.uc.edu>
2018-11-27 08:24:08 -0500

    Initial Commit of Project

---
+++ AllAboutMusic/AllAboutMusic/View/ArtistView.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ArtistView.aspx.cs" Inherits="AllAboutMusic.View.ArtistView" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="Artistname" runat="server"></asp:TextBox>
+        </div>
+        
+        <asp:Button ID="GetArtist" runat="server" OnClick="GetArtist_Click" Text="Go" />
+        <p>
+            <asp:TextBox ID="ShowArtist" runat="server" Height="175px" Width="828px" TextMode="MultiLine"></asp:TextBox>
+        </p>
+    </form>
+</body>
+</html>
---
+++ AllAboutMusic/AllAboutMusic/View/ArtistView.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using AllAboutMusic.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AllAboutMusic.View
+{
+    public partial class ArtistView : System.Web.UI.Page
+    {
+        RestClient client = new RestClient();
+        string domain = "https://musixmatchcom-musixmatch.p.mashape.com/wsr/1.1"; 
+        string getArtistIDURI = "/artist.search?q_artist=artistName&page_size=5";
+        string output = string.Empty;
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GetArtist_Click(object sender, EventArgs e)
+        {
+           
+            client.endpoint = domain + getArtistIDURI.Replace("artistName", Artistname.Text);
+            Console.WriteLine(client.endpoint);
+            output = client.makeRequest();
+
+           if (output != null)
+            {
+                var Artist = JsonConvert.DeserializeObject<dynamic>(output);
+                ShowArtist.Text = Artist.ToString();
+              
+            }
+           
+
+        }
+
+        public List<Artist> GetArtistID(string artist_name)
+        {
+            //ArtistCollection ac;
+            client.endpoint = domain + getArtistIDURI.Replace("artistName", artist_name);       
+            output = client.makeRequest();
+            List <Artist> artlistList = JsonConvert.DeserializeObject<List<Artist>>(output);
+          
+
+            return artlistList;
+
+        }
+        
+    }
+}
